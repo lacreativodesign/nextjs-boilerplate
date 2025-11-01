12e806a03753ec30968a1a2c16275101feac1847
lacreativodesign <lacreativodesign@gmail.com>
2025-10-31 23:46:42 -0700

    Update firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,23 +1,12 @@
-// lib/firebaseAdmin.ts
-import { initializeApp, cert, getApps, App } from "firebase-admin/app";
+import { initializeApp, applicationDefault, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-};
-
-let app: App;
-
 if (!getApps().length) {
-  app = initializeApp({
-    credential: cert(serviceAccount as any)
+  initializeApp({
+    credential: applicationDefault(),
   });
-} else {
-  app = getApps()[0];
 }
 
-export const adminAuth = getAuth(app);
-export const adminDb = getFirestore(app);
+export const adminAuth = getAuth();
+export const adminDb = getFirestore();
