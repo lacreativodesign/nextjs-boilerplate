8c41035401822f194aa4e0c371d4c190a0a1c3a8
lacreativodesign <lacreativodesign@gmail.com>
2025-11-01 22:29:03 -0700

    Change Firebase initialization to use cert method
    Updated Firebase initialization to use service account credentials.

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,10 +1,16 @@
-import { initializeApp, applicationDefault, getApps } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+
 if (!getApps().length) {
   initializeApp({
-    credential: applicationDefault(),
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+    }),
   });
 }
 
