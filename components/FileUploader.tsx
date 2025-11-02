73cae59ba2e16eb9fd770dfb81de252c5aaf93b1
lacreativodesign <lacreativodesign@gmail.com>
2025-11-01 22:29:29 -0700

    Simplify FileUploader component's JSX

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,16 +1,15 @@
+"use client";
 import React from "react";
 import { UploadcareUploader } from "@uploadcare/react-uploader";
 
 export default function FileUploader({ onUpload }) {
   return (
-    <div style={{ padding: "10px", border: "1px dashed #999", borderRadius: 10 }}>
+    <div style={{ padding: 10, border: "1px dashed #999", borderRadius: 10 }}>
       <UploadcareUploader
         pubkey={process.env.NEXT_PUBLIC_UPLOADCARE_PUBLIC_KEY}
         multiple={false}
         onChange={(file) => {
-          if (file?.cdnUrl) {
-            onUpload(file.cdnUrl);
-          }
+          if (file?.cdnUrl) onUpload(file.cdnUrl);
         }}
       />
     </div>
