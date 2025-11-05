d253d9552caa82c8515c153827864b60c2aa76a2
lacreativodesign <lacreativodesign@gmail.com>
2025-11-04 19:13:29 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 // app/page.tsx
-import { redirect } from "next/navigation";
+import React from "react";
+import Link from "next/link";
+
 export default function Home() {
-  redirect("/login");
+  return (
+    <div style={{ padding: 24 }}>
+      <h3>LA CREATIVO ERP</h3>
+      <p><Link href="/login">Go to Login</Link></p>
+    </div>
+  );
 }
