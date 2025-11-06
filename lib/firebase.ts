514c9ac47be2359ef83d1551170dd653b11d02be
lacreativodesign <lacreativodesign@gmail.com>
2025-11-05 20:17:01 -0800

    Create firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,18 @@
+// lib/firebase.ts
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
