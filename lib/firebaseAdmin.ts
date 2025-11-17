94dd0461897fe73742343542269cec41c0dfc98e
lacreativodesign <lacreativodesign@gmail.com>
2025-11-17 09:04:35 +0500

    Update firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,20 +1,10 @@
 import * as admin from "firebase-admin";
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey,
-    }),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY!)),
   });
 }
 
-// THIS IS THE CORRECT FIRESTORE INSTANCE
-export const adminDb = admin.firestore();
-
-// THIS IS THE CORRECT AUTH INSTANCE
 export const adminAuth = admin.auth();
+export const adminDb = admin.firestore();
