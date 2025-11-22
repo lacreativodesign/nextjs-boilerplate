51e733caf16954a255c6fd83e2f8ed0d20c7605e
lacreativodesign <lacreativodesign@gmail.com>
2025-11-22 10:48:56 +0500

    Update layout.tsx

--- app/admin/users/layout.tsx
+++ app/admin/users/layout.tsx
@@ -1,1 +1,11 @@
+import UserTabs from "@/components/users/UserTabs";
+
+export default function UsersLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <UserTabs />
+      {children}
+    </div>
+  );
+}
 
