e41debc7fc2b72db2f591a66d89089f318fa87bd
lacreativodesign <lacreativodesign@gmail.com>
2025-11-28 02:29:34 +0500

    Create head.tsx

---
+++ app/admin/users/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Users | LA CREATIVO ERP</title>
+      <meta
+        name="description"
+        content="View and manage all users in the LA CREATIVO ERP with search, sorting and detailed user profiles."
+      />
+    </>
+  );
+}
